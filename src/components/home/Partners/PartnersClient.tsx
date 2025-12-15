fb6bb361f6e2f71147610516b89d438468a4ebf0
Soh Hong Yu <sohhongyu@gmail.com>
2025-12-15 17:44:27 +0800

    Update PartnersClient.tsx

--- src/components/home/Partners/PartnersClient.tsx
+++ src/components/home/Partners/PartnersClient.tsx
@@ -51,7 +51,7 @@ export default function PartnersClient({
     {
       initialData: initialRows,
       refetchOnWindowFocus: false,
-      staleTime: 1,
+      staleTime: 1000 * 60 * 10,
     }
   );
 
